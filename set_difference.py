776a230c3a96715be2dbe50aa13fe9cbdab3b9a9
Сергей Широков <mr.shirokow@gmail.com>
2022-06-08 19:10:49 +0500

    add typization for variable in last file

--- set_difference.py
+++ set_difference.py
@@ -1,7 +1,7 @@
-known_count = int(input())
-known_words = {input().lower() for i in range(known_count)}
-n = int(input())
-words = {w.lower() for j in range(n) for w in input().split()}
+known_count: int = int(input())
+known_words: set = {input().lower() for i in range(known_count)}
+n: int = int(input())
+words: set = {w.lower() for j in range(n) for w in input().split()}
 
-result = words - known_words
+result: set = words - known_words
 print(*result, sep='\n')
